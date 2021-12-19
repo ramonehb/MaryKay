078c261fedcb67a478a7d9fcc450c2361fbf4380
Humberto Ramone <humbertorody@gmail.com>
2021-12-19 18:24:23 -0300

    Criando a pasta de Scripts SQL

---
+++ DAL/ClienteDAL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ClienteDAL
+    {
+        public bool CadastraCliente(Cliente cliente)
+        {
+            try
+            {
+                using (var db = new BaseDataContext())
+                {
+                    db.Clientes.InsertOnSubmit(cliente);
+                    db.SubmitChanges();
+                    return true;
+                }
+            }
+            catch (Exception erro)
+            {
+                var msg = erro.Message;
+                return false;
+            }
+        }
+    }
+}
