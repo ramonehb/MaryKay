237b99c07852ad2af241ae6f0bd21184f64f6272
Humberto Ramone <humbertorody@gmail.com>
2021-12-21 12:56:31 -0300

    Pasta que contem as validacoes utilizadas em todo o sistema

---
+++ MaryKay/Funcoes/ValidaEmail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaryKay.Funcoes
+{
+    public class ValidaEmail
+    {
+        public bool verificarEmail(string email)
+        {
+            var erro = 0;
+            if (string.IsNullOrEmpty(email))
+            {
+                erro++;
+            }
+            
+            if (!email.Contains(".COM") && !new EmailAddressAttribute().IsValid(email))
+            {
+                erro++;
+            }
+
+            return erro == 0;
+        }
+    }
+}
