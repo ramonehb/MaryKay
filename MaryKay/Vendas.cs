b1690cb687ae47a930a03c87479b4b8448de6369
Humberto Ramone <humbertorody@gmail.com>
2022-03-07 20:09:11 -0300

    Iniciou da tela de Vendas

--- MaryKay/Vendas.cs
+++ MaryKay/Vendas.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,33 @@ namespace MaryKay
         public Vendas()
         {
             InitializeComponent();
+        }
+
+        private void Vendas_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'dataSetProdutosVenda.Produto'. Você pode movê-la ou removê-la conforme necessário.
+            this.produtoTableAdapter.Fill(this.dataSetProdutosVenda.Produto);
+
+        }
+
+        private void btAdicionar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var db = new BaseDataContext())
+                {
+                    var produto = db.Produtos.Single(p => p.ID_Produto == int.Parse(cboProduto.SelectedValue.ToString()));
+                    dgvCarrinhoVenda.DataSource = produto;
+
+                    var refesh = new Vendas();
+                    refesh.ShowDialog();
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+            }
         }
     }
 }
